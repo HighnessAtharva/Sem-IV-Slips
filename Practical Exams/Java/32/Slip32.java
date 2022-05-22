71411fdf871bcd20e6d9e060fc94ca42c191da44
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2022-05-22 15:20:51 +0530

    .

--- Practical Exams/Java/32/Slip32.java
+++ Practical Exams/Java/32/Slip32.java
@@ -2,12 +2,16 @@
 
 import java.util.*;
 
-class InvalidEmailID extends Exception{
+class InvalidEmailID extends Exception {
     InvalidEmailID(String email) {
-     super ("Invalid Email ID Entered. @ missing in "+email) ;
+        super("Invalid Email ID Entered. @ missing in " + email);
     }
- }
- //in all exception handling programs use try catch blocks. Throw the exception is the try block, catch the same exception in the catch block and you will get a non-messy non-cluttered terminal output even when exception occurs. Cleanest way to handle. Never attempt exception handling programs without wrapping risky and error prone statements in the try block Atharva. Remember!
+}
+// in all exception handling programs use try catch blocks. Throw the exception
+// is the try block, catch the same exception in the catch block and you will
+// get a non-messy non-cluttered terminal output even when exception occurs.
+// Cleanest way to handle. Never attempt exception handling programs without
+// wrapping risky and error prone statements in the try block Atharva. Remember!
 
 class Slip32 {
     public static void main(String[] args) throws InvalidEmailID {
@@ -15,16 +19,16 @@ class Slip32 {
         System.out.print("Enter Email: ");
         String email = sc.nextLine(); // reads string
         sc.close();
-        try{
-        if (email.contains("@")) {
-            System.out.println("\nValid Email ID entered!");
-        } else {
-            throw new InvalidEmailID(email);
+        try {
+            if (email.contains("@")) {
+                System.out.println("\nValid Email ID entered!");
+            } else {
+                throw new InvalidEmailID(email);
+            }
+        } 
+        catch (InvalidEmailID e) {
+            System.out.println(e);
         }
-    }
-    catch (InvalidEmailID e){
-        System.out.println(e);
-    }
 
-        }
     }
+}
